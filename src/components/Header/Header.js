3d2b259a55c6c7e3281609bbde589a823035269a
ghosh0311 <tunai2000nov@gmail.com>
2021-11-26 00:10:27 +0530

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,53 @@
+import { TextField, createTheme, ThemeProvider, MenuItem } from "@mui/material";
+
+import React from "react";
+import "./Header.css";
+import categories from "../../data/category";
+
+const Header = ({ setCategory, category, word, setWord, LightMode }) => {
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: LightMode ? "#000" : "#fff",
+      },
+      mode: LightMode ? "light" : "dark",
+    },
+  });
+
+  const handleChange = (language) => {
+    setCategory(language);
+    setWord("");
+  };
+
+  return (
+    <div className="header">
+      <span className="title"> My Dictionary</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField
+            className="search"
+            id="filled-basic"
+            label="Search a Word"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <TextField
+            className="select"
+            select
+            label="Language"
+            value={category}
+            onChange={(e) => handleChange(e.target.value)}
+          >
+            {categories.map((option) => (
+              <MenuItem key={option.label} value={option.label}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/data/category.js
@@ -0,0 +1,12 @@
+const categories = [
+  { value: "English", label: "en" },
+  { value: "Hindi", label: "hi" },
+  { value: "Spanish", label: "es" },
+  { value: "French", label: "fr" },
+  { value: "Japanese", label: "ja" },
+  { value: "German", label: "de" },
+  { value: "Italian", label: "it" },
+  { value: "Korean", label: "ko" },
+];
+
+export default categories;
